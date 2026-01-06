d3997d1ee403fb916bbf8a43fc6206c616871fd8
kkkkarinaaaa <karynavasylyshyna@gmail.com>
2026-01-06 20:54:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 const video1 = document.getElementById('projectVideo1');
-const video2 = document.getElementById('projectVideo2'); // FIX: было 'websiteVideo'
+const video2 = document.getElementById('websiteVideo');
 const video3 = document.getElementById('projectVideo3');
 
 // Sidebar elements //
 const sideBar = document.querySelector('.sidebar');
 const menu = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.close-icon');
+const closeIcon = document.querySelector('.close-icon')
+
 
 const hoverSign = document.querySelector('.hover-sign');
 
-const videoList = [video1, video2, video3].filter(Boolean);
+const videoList =[video1, video2, video3];
 
-videoList.forEach(function (video) {
-  video.addEventListener("mouseover", function () {
-    // autoplay policies: видео должно быть muted в HTML (ты уже добавила)
-    video.play().catch(() => {});
-    if (hoverSign) hoverSign.classList.add("active");
-  });
-
-  video.addEventListener("mouseout", function () {
+videoList.forEach (function(video){
+    video.addEventListener("mouseover", function(){
+        video.play()
+        hoverSign.classList.add("active")
+    })
+    video.addEventListener("mouseout", function(){
     video.pause();
-    if (hoverSign) hoverSign.classList.remove("active");
-  });
-});
+    hoverSign.classList.remove("active")
+})
+})
 
 // Sidebar elements //
-if (menu && sideBar) {
-  menu.addEventListener("click", function () {
-    sideBar.classList.remove("close-sidebar");
-    sideBar.classList.add("open-sidebar");
-  });
-}
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
+})
 
 if (closeIcon && sideBar) {
   closeIcon.addEventListener("click", function () {
@@ -38,3 +41,4 @@ if (closeIcon && sideBar) {
     sideBar.classList.add("close-sidebar");
   });
 }
+
